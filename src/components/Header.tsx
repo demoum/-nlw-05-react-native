74dbbbd8ad50d75d0e650c8dc277aa22930103d2
Matheus Moura <60050085+demoum@users.noreply.github.com>
2021-04-24 21:42:35 -0300

    Project NLW5 ReactNative

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+import userImg from "../assets/matheus.png";
+import colors from "../styles/colors";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import fonts from "../styles/fonts";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export function Header() {
+  const [userName, setUserName] = useState<string>();
+
+  useEffect(() => {
+    async function loadStorageUserName() {
+      const user = await AsyncStorage.getItem("@plantmanager:user");
+      setUserName(user || "");
+    }
+
+    loadStorageUserName();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>{userName}</Text>
+      </View>
+
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+});
